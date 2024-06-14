8d816a9e741d5f816fdfe729ed3fc417eb629c6e
Vanshika Verma <105100350+vanshikaverma13@users.noreply.github.com>
2024-06-14 20:51:19 +0530

    Added Hero Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import styles from './App.module.css'
 import { Navbar } from './components/Navbar/Navbar'
-
+import { Hero } from './components/Hero/Hero'
 function App() {
   return (
     <div className={styles.app}>
       <Navbar />
+      <Hero/>
     </div>
   )
 }

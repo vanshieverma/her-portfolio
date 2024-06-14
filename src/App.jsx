427702b2ecb134763de47078b286b352ade9ec31
Vanshika Verma <105100350+vanshikaverma13@users.noreply.github.com>
2024-06-14 12:05:39 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import styles from './App.module.css'
+import { Navbar } from './components/Navbar/Navbar'
+
+function App() {
+  return (
+    <div className={styles.app}>
+      <Navbar />
+    </div>
+  )
+}
+
+export default App
